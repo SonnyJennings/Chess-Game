9935eb05e921fb96b723fee02a403f13f9c95d1b
SonnyJennings <120763516+SonnyJennings@users.noreply.github.com>
2024-04-07 22:45:14 -0700

    Update Menu.h

--- include/Menu.h
+++ include/Menu.h
@@ -1,9 +1,5 @@
 /*!
  * \file Menu.h
- * \author Orion Serup (oserup@uci.edu)
- * \author Ian Poremba (iporemba@uci.edu)
- * \author Raymond Duong (duongr2@uci.edu)
- * \author Chris Rodriguez (chriar10@uci.edu)
  * \author Sunshine Jennings (smjennin@uci.edu)
  * \brief Contains the prototypes and custom types for the Menu Module of Ultimate Chess
  * \version 1.0
