1e2d3a824719d01777198ef58f4ff8ea89eb47da
SonnyJennings <120763516+SonnyJennings@users.noreply.github.com>
2024-04-07 22:45:04 -0700

    Update Gameplay.h

--- include/Gameplay.h
+++ include/Gameplay.h
@@ -1,9 +1,5 @@
 /*!
  * \file Gameplay.h
- * \author Orion Serup (oserup@uci.edu)
- * \author Ian Poremba (iporemba@uci.edu)
- * \author Raymond Duong (duongr2@uci.edu)
- * \author Chris Rodriguez (chriar10@uci.edu)
  * \author Sunshine Jennings (smjennin@uci.edu)
  * \brief Contains the prototypes and custom types for the Gameplay Module of Ultimate Chess
  * \version 1.0
